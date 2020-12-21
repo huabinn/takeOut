ab7366975084d38143c51788fe53955f1d236dba
binn <binn@qq.com>
2020-12-21 21:43:46 +0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <router-view/>
-    <FooterGuide/>
+    <FooterGuide v-if="$route.meta.showFooter"/>
   </div>
 </template>
 
--- src/pages/Profile/Profile.vue
+++ src/pages/Profile/Profile.vue
@@ -1,95 +1,95 @@
 <template>
-    <section class="profile">
-        <HeaderTop title="我的">
-        </HeaderTop>
-        <section class="profile-number">
-          <a href="javascript:" class="profile-link">
-            <div class="profile_image">
-              <i class="iconfont icon-bussiness-man-fill"></i>
-            </div>
-            <div class="user-info">
-              <p class="user-info-top">登录/注册</p>
-              <p>
-                <span class="user-icon">
-                  <i class="iconfont icon-mobile-phone"></i>
-                </span>
-                <span class="icon-mobile-number">暂无绑定手机号</span>
-              </p>
-            </div>
-            <span class="arrow">
-              <i class="iconfont icon-arrow-right"></i>
+  <section class="profile">
+    <HeaderTop title="我的">
+    </HeaderTop>
+    <section class="profile-number">
+      <router-link to="/login" class="profile-link">
+        <div class="profile_image">
+          <i class="iconfont icon-bussiness-man-fill"></i>
+        </div>
+        <div class="user-info">
+          <p class="user-info-top">登录/注册</p>
+          <p>
+            <span class="user-icon">
+              <i class="iconfont icon-mobile-phone"></i>
             </span>
-          </a>
-        </section>
-        <section class="profile_info_data border-1px">
-          <ul class="info_data_list">
-            <a href="javascript:" class="info_data_link">
-              <span class="info_data_top"><span>0.00</span>元</span>
-              <span class="info_data_bottom">我的余额</span>
-            </a>
-            <a href="javascript:" class="info_data_link">
-              <span class="info_data_top"><span>0</span>个</span>
-              <span class="info_data_bottom">我的优惠</span>
-            </a>
-            <a href="javascript:" class="info_data_link">
-              <span class="info_data_top"><span>0</span>分</span>
-              <span class="info_data_bottom">我的积分</span>
-            </a>
-          </ul>
-        </section>
-        <section class="profile_my_order border-1px">
-          <!-- 我的订单 -->
-          <a href='javascript:' class="my_order">
-            <span>
-              <i class="iconfont icon-suggest"></i>
-            </span>
-            <div class="my_order_div">
-              <span>我的订单</span>
-              <span class="my_order_icon">
-                <i class="iconfont icon-arrow-right"></i>
-              </span>
-            </div>
-          </a>
-          <!-- 积分商城 -->
-          <a href='javascript:' class="my_order">
-            <span>
-              <i class="iconfont icon-extra-inquiries"></i>
-            </span>
-            <div class="my_order_div">
-              <span>积分商城</span>
-              <span class="my_order_icon">
-                <i class="iconfont icon-arrow-right"></i>
-              </span>
-            </div>
-          </a>
-          <!-- 硅谷外卖会员卡 -->
-          <a href="javascript:" class="my_order">
-            <span>
-              <i class="iconfont icon-brand"></i>
-            </span>
-            <div class="my_order_div">
-              <span>硅谷外卖会员卡</span>
-              <span class="my_order_icon">
-                <i class="iconfont icon-arrow-right"></i>
-              </span>
-            </div>
-          </a>
-        </section>
-        <section class="profile_my_order border-1px">
-          <!-- 服务中心 -->
-          <a href="javascript:" class="my_order">
-            <span>
-              <i class="iconfont icon-company"></i>
-            </span>
-            <div class="my_order_div">
-              <span>服务中心</span>
-              <span class="my_order_icon">
-                <i class="iconfont icon-arrow-right"></i>
-              </span>
-            </div>
-          </a>
-        </section>
+            <span class="icon-mobile-number">暂无绑定手机号</span>
+          </p>
+        </div>
+        <span class="arrow">
+          <i class="iconfont icon-arrow-right"></i>
+        </span>
+      </router-link>
     </section>
+    <section class="profile_info_data border-1px">
+      <ul class="info_data_list">
+        <a href="javascript:" class="info_data_link">
+          <span class="info_data_top"><span>0.00</span>元</span>
+          <span class="info_data_bottom">我的余额</span>
+        </a>
+        <a href="javascript:" class="info_data_link">
+          <span class="info_data_top"><span>0</span>个</span>
+          <span class="info_data_bottom">我的优惠</span>
+        </a>
+        <a href="javascript:" class="info_data_link">
+          <span class="info_data_top"><span>0</span>分</span>
+          <span class="info_data_bottom">我的积分</span>
+        </a>
+      </ul>
+    </section>
+    <section class="profile_my_order border-1px">
+      <!-- 我的订单 -->
+      <a href='javascript:' class="my_order">
+        <span>
+          <i class="iconfont icon-suggest"></i>
+        </span>
+        <div class="my_order_div">
+          <span>我的订单</span>
+          <span class="my_order_icon">
+            <i class="iconfont icon-arrow-right"></i>
+          </span>
+        </div>
+      </a>
+      <!-- 积分商城 -->
+      <a href='javascript:' class="my_order">
+        <span>
+          <i class="iconfont icon-extra-inquiries"></i>
+        </span>
+        <div class="my_order_div">
+          <span>积分商城</span>
+          <span class="my_order_icon">
+            <i class="iconfont icon-arrow-right"></i>
+          </span>
+        </div>
+      </a>
+      <!-- 广药外卖会员卡 -->
+      <a href="javascript:" class="my_order">
+        <span>
+          <i class="iconfont icon-brand"></i>
+        </span>
+        <div class="my_order_div">
+          <span>广药外卖会员卡</span>
+          <span class="my_order_icon">
+            <i class="iconfont icon-arrow-right"></i>
+          </span>
+        </div>
+      </a>
+    </section>
+    <section class="profile_my_order border-1px">
+      <!-- 服务中心 -->
+      <a href="javascript:" class="my_order">
+        <span>
+          <i class="iconfont icon-company"></i>
+        </span>
+        <div class="my_order_div">
+          <span>服务中心</span>
+          <span class="my_order_icon">
+            <i class="iconfont icon-arrow-right"></i>
+          </span>
+        </div>
+      </a>
+    </section>
+  </section>
 </template>
 
 <script>
