cf5235a185d8c11e4163293254426c717684ce7e
binn <binn@qq.com>
2020-12-21 16:37:36 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
+// 入口js
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  el: '#app',
+  render: h => h(App),
+  router
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+import MSite from '../pages/MSite/MSite'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
+export default new VueRouter({
+    routes: [
+        {
+            path: '/msite',
+            component: MSite
+        },
+        {
+            path: '/search',
+            component: Search
+        },
+        {
+            path: '/order',
+            component: Order
+        },
+        {
+            path: '/profile',
+            component: Profile
+        },
+        {
+            path: '/',
+            redirect: '/msite'
+        }
+    ]
 })
-
-export default router
